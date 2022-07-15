bad389b209f25071422035a0dd4c226439b376ee
goyal123 <akhilg642@gmail.com>
2022-07-15 17:37:38 +0530

    Calculating Length of Line

---
+++ Line_Length/Line_Length.cs
@@ -0,0 +1,13 @@
+﻿namespace Line_Comparision_App
+{
+    public class Line_Length
+    {
+        public static void LineLength(int x1,int x2,int y1,int y2)
+        {
+            double length;
+            length = Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2);
+            Console.WriteLine("Length of Line = "+Math.Sqrt(length));
+
+        }
+    }
+}
