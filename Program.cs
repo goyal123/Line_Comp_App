2362db0b60670051c6a2337a28121873257a86d4
goyal123 <akhilg642@gmail.com>
2022-07-15 16:33:51 +0530

    Welcome message on Line comparision program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Line_Comparision_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
